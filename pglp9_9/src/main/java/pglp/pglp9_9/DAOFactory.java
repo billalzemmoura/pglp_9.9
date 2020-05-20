9fa73ee0027cb1856a9d1cb9075cf55e2e3b1ef1
billal zemmoura <billal.zemmoura@ens.uvsq.fr>
2020-05-20 20:41:08 +0200

    ajouts des DAO

---
+++ pglp9_9/src/main/java/pglp/pglp9_9/DAOFactory.java
@@ -0,0 +1,16 @@
+package pglp.pglp9_9;
+
+public class DAOFactory {
+ 
+
+public static  DAO <Forme> getFormDaoDerby() throws Exception{
+    	
+    	return new DaoDerbyForme();
+    } 
+
+public static  CompositeDaoDerby getCompositeDaoDerby() throws Exception{
+    	
+    	return new CompositeDaoDerby();
+    } 
+
+}
