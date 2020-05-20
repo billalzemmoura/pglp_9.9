cb24401acaa0ff39db797f7a7465243e3bdefea4
billal zemmoura <billal.zemmoura@ens.uvsq.fr>
2020-05-20 20:34:22 +0200

    ajouts de la class dessin la classe mere forme

---
+++ pglp9_9/src/main/java/pglp/pglp9_9/Forme.java
@@ -0,0 +1,20 @@
+package pglp.pglp9_9;
+
+public abstract class  Forme {
+	String nomGRP="";
+public String getNomGRP() {
+		return nomGRP;
+	}
+	public void setNomGRP(String nomGRP) {
+		this.nomGRP = nomGRP;
+	}
+String nom;
+	public String getNom() {
+	return nom;
+}
+public void setNom(String nom) {
+	this.nom = nom;
+}
+	void move(int  X,int Y) {
+	}
+}
